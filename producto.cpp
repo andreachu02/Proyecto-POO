73dace9c599f10d846e32c6ba308b9e1832b2bd8
andreachu02 <51957242+andreachu02@users.noreply.github.com>
2019-11-26 12:31:42 -0500

    Update producto.cpp

--- producto.cpp
+++ producto.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 #include "producto.h"
+#include "tipo.h"
+#include "slot.h"
 
-Cproducto::producto(string nombre, int slot):tipo{nombre}, slot_posicion{slot}{
-
+Cproducto::Cproducto(std::string nombre, al x, al y):tipo{nombre}, prod_pos_x{x}, prod_pos_y{y}{
+  //  Cslot->prods.push_back(Cproducto); (añadir producto a vector prods en slot)
 }
+
+
+void Cproducto::set_px(al x)
+  {
+    prod_pos_x = x;
+}
+  
+void Cproducto::set_py(al y)
+  {
+    prod_pos_x = y;
+}
+
+al Cproducto::get_px()
+  {
+    return prod_pos_x;
+}
+
+al Cproducto::get_py()
+  {
+    return prod_pos_y;
+}
+
+
 
