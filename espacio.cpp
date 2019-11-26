74f33039782f6eb951e54c7b759d2f41fcc54503
andreachu02 <51957242+andreachu02@users.noreply.github.com>
2019-11-26 12:30:25 -0500

    Update espacio.cpp

--- espacio.cpp
+++ espacio.cpp
@@ -1,15 +1,48 @@
+
 #include <iostream>
+#include <vector>
 #include "espacio.h"
-#include"slots.h"
+#include "slot.h"
+#include "robot.h"
 
+using namespace std;
 
-Calmacenamiento::almacenamiento(int largo, int ancho): largo_total{largo}, ancho_total{ancho}:
-         {
-    for (auto &posicion_x: slots)
-        for (int i = 0; i < posicion_y; i++) {
-            //row.push_back(casilla_t(hc, wc));
-            posicion_x.emplace_back(hc, wc);
+Calmacenamiento::Calmacenamiento(al largo, al ancho): largo_total{largo}, ancho_total{ancho} {
+ 
+    for (int i = 1; i <= ancho_total; i++) {
+        for(int j=1; j<= largo_total; j++){
+            Cslot s(i, j);
+            slots.push_back(s);
         }
+    }
+
+    /*
+    Cslot* s = new s(x, y);
+    slots.push_back(*s);
+    delete s;
+    */
+
+}
+
+void Calmacenamiento::imprimir_almacenamiento() {
+    for(int i=0; i<ancho_total; i++){
+        for(int j=0; j<largo_total; j++){
+          for(int k=0;k<robots.size();k++){
+            if(robots[k].get_rob_x() == i && robots[k].get_rob_y() == j){
+              cout<<" O ";
+            }
+            else cout<<" . "; 
+          }
+        }
+        cout<<endl;
+    }
+}
+
+//
+
+void Calmacenamiento::add_robot(Crobot &r){
+  robots.push_back(r);
+}
 
 
 
