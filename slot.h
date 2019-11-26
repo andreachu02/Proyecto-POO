27efa85c7ceacc1afa822195790182a004ebbd81
andreachu02 <51957242+andreachu02@users.noreply.github.com>
2019-11-26 12:35:36 -0500

    Update slot.h

--- slot.h
+++ slot.h
@@ -2,25 +2,33 @@
 #ifndef PROYECTO_POO_SLOT
 #define PROYECTO_POO_SLOT
 
+#include <vector>
 #include "tipo.h"
-
+#include "producto.h"
 
 class Cslot{
   //priv
-  num posicion_slot;
-  num max_slot;
-  string producto;
-  bool ocupado_producto;
+  num posicion_slot_x;
+  num posicion_slot_y;
+
+  bool ocupado_producto; //true cuando hay un producto en el slot
   bool ocupado_robot;
-  
+  vector<Cproducto> &prods;
+
   public:
   //atributos
-  slot();
-  slot(int posicion_x, int posicion_y);
+  Cslot(int posicion_x, int posicion_y);
    //métodos
-  get_condicion();
+  int get_x();
+  int get_y();
+  bool get_condicion_slot();
+  bool get_condicion_robot();
 
-}
+
+};
+
+
 
 
 #endif 
+
