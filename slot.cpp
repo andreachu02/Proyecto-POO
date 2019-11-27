5d7b29e3f74b9a620ce474b53adb689f0a879837
andreachu02 <51957242+andreachu02@users.noreply.github.com>
2019-11-27 11:12:42 -0500

    Update slot.cpp

--- slot.cpp
+++ slot.cpp
@@ -1,54 +1,41 @@
 
-
 #include "slot.h"
-#include "espacio.h"
-#include "robot.h"
 
-Cslot::Cslot(int posicion_x, int posicion_y): posicion_slot_x{posicion_x},
-posicion_slot_y{posicion_y} {
-  ocupado_robot = false;
-  ocupado_producto = false;
+
+Cslot::Cslot(int x, int y): slot_x{x},
+slot_y{y} {
+  vacio_producto = true;
+  vacio_robot = true;
+  p_en_slot = nullptr;
+
 }
 
-
-int Cslot::get_x(){
-  return posicion_slot_x;
+tipo Cslot::get_s_x(){
+  return slot_x;
 }
 
-int Cslot::get_y()
-  {
-    return posicion_slot_y;
+tipo Cslot::get_s_y(){
+  return slot_y;
 }
 
-bool Cslot::get_condicion_slot(){ 
-
-  for(int i=1; i<=Calmacenamiento->largo_total; i++){
-   for(int j=1; j<=Calmacenamiento->ancho_total; j++){
-   if(posicion_slot_x == Calmacenamiento->slots[i].get_x && posicion_slot_y == Calmacenamiento->slots[i].get_y){
-     if(prods.size() == 0) ocupado_producto =false;
-     else ocupado_producto = true;
-    }
-   }
- }
-  
-   return ocupado_producto;
+void Cslot::agrega_producto(Cproducto* p){
+  p_en_slot = p;
+  vacio_producto = false;
 }
 
-
-bool Cslot::get_condicion_robot(){
-   for(int i=1; i<=Calmacenamiento.largo_total; i++){
-    for(int j=1; j<=Calmacenamiento.ancho_total; j++){
-      for(int k=0; k<Calmacenamiento->robots.size(); k++){
-      if(posicion_slot_x == robots[k].get_rob_x() && posicion_slot_y == robots[k]->get_rob_y()){
-          ocupado_robot = true;
-        
-          }
-        else ocupado_robot = false;
-        }
-      }
-    } 
-    return ocupado_robot;
+void Cslot::saca_producto(){
+  p_en_slot = nullptr;
 }
 
+string Cslot::get_producto(){
+  if(vacio_producto == false){
 
+    return p_en_slot->get_name();
+  }
+  else return "No hay un producto";
+}
 
+Cproducto* Cslot::get_ptr_producto()
+{
+  return p_en_slot;
+}
